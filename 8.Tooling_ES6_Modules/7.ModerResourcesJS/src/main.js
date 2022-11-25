e26147ae1764cec28260ea11d22b76a1b99159e1
Thiago Viana <tvm.dev2015@gmail.com>
2022-11-25 13:57:13 -0300

    Modern Resources JS class done

---
+++ 8.Tooling_ES6_Modules/7.ModerResourcesJS/src/main.js
@@ -0,0 +1,14 @@
+//js for old browsers:
+import 'regenerator-runtime/runtime' 
+import 'core-js/stable'
+
+
+
+
+//js imports:
+import execute from './modules/promises'
+
+//css imports:
+import './assets/css/style.css'
+
+execute()
