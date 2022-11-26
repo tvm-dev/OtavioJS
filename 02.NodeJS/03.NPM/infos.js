0261689b0bf97a6a4fe9feda24a359a73a6e8b7a
Thiago Viana <tvm.dev2015@gmail.com>
2022-11-26 19:54:20 -0300

    npm

---
+++ 02.NodeJS/03.NPM/infos.js
@@ -0,0 +1,17 @@
+/**
+ * npm install express -E => install always the exact version. Do not use '^'
+
+* Install specific version from package: npm i express@2.1.0
+
+
+infos versions packages:
+
+ex.: version 2(major).1(minor).0(patch)
+major: change everthing, breack the compatibili
+minor: theare are changes but this is compatiple
+
+'~' => only change the path version!
+
+
+
+*/
