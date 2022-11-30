961f424724a3e6cbfb25b1f8caea9f3b18a56274
Thiago Viana <tvm.dev2015@gmail.com>
2022-11-30 19:14:35 -0300

    nodemon class done

--- 02.NodeJS/6.express/server.js
+++ 02.NodeJS/6.express/server.js
@@ -2,12 +2,24 @@ const express = require('express')
 const server = express()
 
 server.get('/', (req, res) =>{
-    res.send('Hello World...')
+    res.send(`
+        <form action="/" method="post">
+        Name:<input type="text" name="name">
+        <button>Send</button>
+        </form>
+
+    `)
 });
 
 server.get('/contato', (req, res) => {
     res.send('Thanks for contact us!')
 })
+
+server.post('/', (req, res) => {
+    res.send('Your form submit was received.')
+})
+
+
 
 server.listen(3000, () => {
     console.log('Server running on port 3000, at adreess http://localhost:3000')
