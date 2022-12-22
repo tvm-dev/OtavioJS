bfc3067ebf55912b481ef621a77e28858bff70c8
Thiago Viana <tvm.dev2015@gmail.com>
2022-12-22 11:18:38 -0300

    Editting tasks

--- 05.React/todo/src/components/Main.js
+++ 05.React/todo/src/components/Main.js
@@ -11,7 +11,7 @@ export default class Main extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const { tasks } = this.state;
+    const { tasks, index } = this.state;
     let { newTask } = this.state;
     newTask = newTask.trim();
 
@@ -19,19 +19,34 @@ export default class Main extends Component {
 
     const newTasks = [...tasks];
 
-    this.setState({
-      tasks: [...newTasks, newTask],
-    });
+    if (index === -1) {
+      this.setState({
+        tasks: [...newTasks, newTask],
+        newTask: '',
+
+      });
+    } else {
+      newTasks[index] = newTask;
+
+      this.setState({
+        tasks: [...newTasks],
+        index: -1,
+      });
+    }
   };
 
-  inputChanged = (e) => {
+  handleChange = (e) => {
     this.setState({
       newTask: e.target.value,
     });
   };
 
   handleEdit = (e, index) => {
-    console.log('Edit', index);
+    const { tasks } = this.state;
+    this.setState({
+      index,
+      newTask: tasks[index],
+    });
   };
 
   handleDelete = (e, index) => {
@@ -51,7 +66,7 @@ export default class Main extends Component {
         <h1>TODO List</h1>
         <form onSubmit={this.handleSubmit} action="#" className="form">
           <input
-            onChange={this.inputChanged}
+            onChange={this.handleChange}
             type="text"
             value={newTask}
           />
