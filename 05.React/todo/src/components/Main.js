2ce6ee05f05bf0b9e60669a4d5add5a11c9c4aa3
Thiago Viana <tvm.dev2015@gmail.com>
2022-12-21 22:33:57 -0300

    Styling form, done

--- 05.React/todo/src/components/Main.js
+++ 05.React/todo/src/components/Main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { FaPlus } from 'react-icons/fa'; // to Form
 import './Main.css';
 
 export default class Main extends Component {
@@ -13,12 +14,19 @@ export default class Main extends Component {
   };
 
   render() {
+    const { newTask } = this.state;
     return (
       <div className="main">
-        <h1> TODO List</h1>
-        <form action="#">
-          <input onChange={this.inputChanged} type="text" />
-          <button type="submit">Send</button>
+        <h1>TODO List</h1>
+        <form action="#" className="form">
+          <input
+            onChange={this.inputChanged}
+            type="text"
+            value={newTask}
+          />
+          <button type="submit">
+            <FaPlus />
+          </button>
 
         </form>
       </div>
